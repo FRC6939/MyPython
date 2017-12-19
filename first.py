4324e22961b1ab380a9335fb0531cf24c620cc44
FRCTeamCode <Kai0314@hotmail.com>
2017-12-19 22:32:17 +0800

    Revert "revert"
    This reverts commit 7cca23fe197a10ce00ef091e00d2667a7f7f1a8c.

--- first.py
+++ first.py
@@ -2,26 +2,11 @@ print("Hello World!")
 fruit = ['a', 'b', 'c']
 for fru in fruit:
     print("H " + fru + " A")
-print("Hello World!")
-print("Hello World!")
-x = 9
-if x<5:
+x = 2
+if  x < 5:
     print("small")
-if x>5 and x<10:
+elif x>5 and x<10:
     print("large")
-print(3/2)
-print("Hello World!")
-fruit = ['a', 'b', 'c']
-for fru in fruit:
-    print("H " + fru + " A")
-print("Hello World!")
-print("Hello World!")
-x = 9
-if x<5:
-    print("small")
-if x>5 and x<10:
-    print("large")
-<<<<<<< HEAD
 else:print(3/2)
 
 cars = ['bmw', 'audi', 'toyato','BYD']
@@ -37,6 +22,3 @@ print(squares);print(sum(squares));squares.reverse();print(squares)
 a=1;b=10;c=20;print(a>b)
 
 x=10;y=20
-=======
-print(3/2)
->>>>>>> parent of 16dd4f8... update
--- my.java
+++ my.java
@@ -1,6 +1,8 @@
-public class hello
-{
+public class my{
   public static void main(String args[]){
     System.out.println("hello!");
+    for(int i=0;i<10;i++){
+        System.out.println(i);
+    }
   }
 }
