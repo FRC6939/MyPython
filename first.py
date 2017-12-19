08c3fa12216cee8cf6f52088af59e09601ac7523
FRCTeamCode <Kai0314@hotmail.com>
2017-12-19 22:34:14 +0800

    Revert "test"
    This reverts commit a8fb1580492fb267c9e85625f0d880f850a0d636.

--- first.py
+++ first.py
@@ -1,24 +1,5 @@
 print("Hello World!")
 fruit = ['a', 'b', 'c']
 for fru in fruit:
-    print("H " + fru + " A")
+    print(fru)
 print("Hello World!")
-print("Hello World!")
-x = 9
-if x<5:
-    print("small")
-if x>5 and x<10:
-    print("large")
-print(3/2)
-print("Hello World!")
-fruit = ['a', 'b', 'c']
-for fru in fruit:
-    print("H " + fru + " A")
-print("Hello World!")
-print("Hello World!")
-x = 9
-if x<5:
-    print("small")
-if x>5 and x<10:
-    print("large")
-print(3/2)
