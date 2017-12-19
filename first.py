70b9ba0f701e9695c59f495ed9c9483dcd3dde09
FRCTeamCode <Kai0314@hotmail.com>
2017-12-19 22:32:01 +0800

    Revert "Revert "update""
    This reverts commit 3a53855b79e82cb394839110886f8c3d78067486.

--- first.py
+++ first.py
@@ -6,26 +6,10 @@ print("Hello World!")
 print("Hello World!")
 x = 9
 if x<5:
-<<<<<<< HEAD
-=======
     print("small")
 if x>5 and x<10:
     print("large")
 print(3/2)
-print("Hello World!")
-fruit = ['a', 'b', 'c']
-for fru in fruit:
-    print("H " + fru + " A")
-print("Hello World!")
-print("Hello World!")
-x = 9
-if x<5:
->>>>>>> parent of 16dd4f8... update
-    print("small")
-if x>5 and x<10:
-    print("large")
-print(3/2)
-<<<<<<< HEAD
 print("Hello World!")
 fruit = ['a', 'b', 'c']
 for fru in fruit:
@@ -55,6 +39,4 @@ a=1;b=10;c=20;print(a>b)
 x=10;y=20
 =======
 print(3/2)
->>>>>>> parent of 16dd4f8... update
-=======
 >>>>>>> parent of 16dd4f8... update
