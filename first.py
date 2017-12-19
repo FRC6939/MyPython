77db1ad5d4994dd31cecceaa1d48ebb330a8ff2e
FRCTeamCode <Kai0314@hotmail.com>
2017-12-19 22:37:09 +0800

    update2

--- first.py
+++ first.py
@@ -1,5 +1,35 @@
 print("Hello World!")
 fruit = ['a', 'b', 'c']
 for fru in fruit:
-    print(fru)
+    print("H " + fru + " A")
 print("Hello World!")
+print("Hello World!")
+x = 9
+if x<5:
+    print("small")
+if x>5 and x<10:
+    print("large")
+print(3/2)
+print("Hello World!")
+fruit = ['a', 'b', 'c']
+for fru in fruit:
+    print("H " + fru + " A")
+print("Hello World!")
+print("Hello World!")
+x = 9
+if x<5:
+    print("small")
+if x>5 and x<10:
+    print("large")
+print(3/2)
+cars = ['bmw', 'audi', 'toyato','BYD']
+print(cars);print(sorted(cars));print(cars[0:2]);print(len(cars))
+for car in cars:
+    print(car)
+print('bmw' in cars)
+
+squares = [5]
+for value in range(1,10,2):squares.append(value**3);
+print(squares);print(sum(squares));squares.reverse();print(squares)
+
+a=1;b=10;print(a>b)
